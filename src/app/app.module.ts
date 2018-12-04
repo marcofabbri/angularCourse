10357b18259df6678400a90611b06ee0c17562b9
Marco Fabbri <marco.fabbri@drwolf.it>
2018-12-04 22:34:00 +0100

    :sparkles: structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TokenHeaderInterceptorComponent } from './token-header-interceptor/token-header-interceptor.component';
 import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     TokenHeaderInterceptorComponent,
     HighlightDirective,
+    UnlessDirective,
   ],
   imports: [
     BrowserModule,
